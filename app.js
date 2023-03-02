ddfdb3afc1756211f1fc3fe765fe18da0fe653a8
JustTrzcina <andrzej.maciaszek009@gmail.com>
2023-03-02 20:54:46 +0100

    add GET and POST functionality

--- app.js
+++ app.js
@@ -11,6 +11,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+const articleSchema = mongoose.Schema({
+    title:String,
+    content:String
+})
+
+const Article = mongoose.model("Article",articleSchema);
+
+app.get("/articles",function(req,res){
+    async function getAllArticles(){
+        await mongoose.connect(mongoURI);
+        const result = await Article.find();
+        await mongoose.connection.close();
+        return result
+    }
+    getAllArticles().then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        res.send(err);
+    });
+})
+
+app.post("/articles",function(req,res){
+    const article = new Article({
+        title:req.body.title,
+        content:req.body.content
+    })
+    async function postArticle(){
+        await mongoose.connect(mongoURI);
+        await article.save();
+        await mongoose.connection.close();
+    }
+    postArticle().then(()=>{
+        res.send("Article sucessfully added");
+    }).catch((err)=>{
+        res.send(err);
+    });
+});
+
+
 app.listen(3000,function(){
     console.log("Server running on port 3000");
 })
