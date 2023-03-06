643d33bc9e838987a990c9a5935ec95737d7ecdc
JustTrzcina <andrzej.maciaszek009@gmail.com>
2023-03-06 19:47:54 +0100

    add route chaining and finish RESTful interface

--- app.js
+++ app.js
@@ -18,37 +18,114 @@ const articleSchema = mongoose.Schema({
 
 const Article = mongoose.model("Article",articleSchema);
 
-app.get("/articles",function(req,res){
-    async function getAllArticles(){
-        await mongoose.connect(mongoURI);
-        const result = await Article.find();
-        await mongoose.connection.close();
-        return result
-    }
-    getAllArticles().then((result)=>{
-        res.send(result);
-    }).catch((err)=>{
-        res.send(err);
-    });
-})
 
-app.post("/articles",function(req,res){
-    const article = new Article({
-        title:req.body.title,
-        content:req.body.content
+app.route("/articles")
+    .get(function(req,res){
+        async function getAllArticles(){
+            await mongoose.connect(mongoURI);
+            const result = await Article.find();
+            await mongoose.connection.close();
+            return result
+        }
+        getAllArticles().then((result)=>{
+            res.send(result);
+        }).catch((err)=>{
+            res.send(err);
+        });
     })
-    async function postArticle(){
-        await mongoose.connect(mongoURI);
-        await article.save();
-        await mongoose.connection.close();
-    }
-    postArticle().then(()=>{
-        res.send("Article sucessfully added");
-    }).catch((err)=>{
-        res.send(err);
-    });
-});
+    .post(function(req,res){
+        const article = new Article({
+            title:req.body.title,
+            content:req.body.content
+        })
+        async function postArticle(){
+            await mongoose.connect(mongoURI);
+            await article.save();
+            await mongoose.connection.close();
+        }
+        postArticle().then(()=>{
+            res.send("Article sucessfully added");
+        }).catch((err)=>{
+            res.send(err);
+        });
 
+    })
+    .delete(function(req,res){
+        async function deleteArticles(){
+            await mongoose.connect(mongoURI);
+            await Article.deleteMany()
+            await mongoose.connection.close();
+        }
+        deleteArticles().then(()=>{
+            res.send("Articles deleted");
+        }).catch((err)=>{
+            res.send(err);
+        });
+    });
+
+
+app.route("/articles/:articleTitle")
+    .get(function(req,res){
+        const requestedTitle = req.params.articleTitle
+        async function getOneArticle(){
+            await mongoose.connect(mongoURI);
+            const result = await Article.findOne({title:requestedTitle});
+            await mongoose.connection.close();
+            return result
+        }
+        getOneArticle().then((result)=>{
+            res.send(result);
+        }).catch((err)=>{
+            res.send(err);
+        });
+    })
+    .put(function(req,res){
+        const requestedTitle = req.params.articleTitle
+        const newTitle = req.body.newTitle;
+        const newContent = req.body.newContent;
+        async function updateArticle(){
+            await mongoose.connect(mongoURI);
+            const article = await Article.findOne({title:requestedTitle});
+            await article.overwrite({title:newTitle, content:newContent});
+            await article.save();
+            await mongoose.connection.close();
+        }
+        updateArticle().then(()=>{
+            res.send("Update succesfull");
+        }).catch((err)=>{
+            res.send(err);
+        });
+    })
+    .patch(function(req,res){
+        const requestedTitle = req.params.articleTitle
+        async function patchArticle(){
+            await mongoose.connect(mongoURI);
+            await Article.updateMany(
+                {title: requestedTitle},
+                {$set: req.body}
+            )
+            await mongoose.connection.close();
+        }
+        patchArticle().then(()=>{
+            res.send("Patch succesfull");
+        }).catch((err)=>{
+            res.send(err);
+        });
+    })
+    .delete(function(req,res){
+        const requestedTitle = req.params.articleTitle;
+        async function deleteOneArticle(){
+            await mongoose.connect(mongoURI);
+            await Article.deleteOne({title:requestedTitle})
+            await mongoose.connection.close();
+        }
+        deleteOneArticle().then(()=>{
+            res.send("Article deleted");
+        }).catch((err)=>{
+            res.send(err);
+        });
+
+    })
 
 app.listen(3000,function(){
     console.log("Server running on port 3000");
